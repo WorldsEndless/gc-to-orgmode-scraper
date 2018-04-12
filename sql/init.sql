4032aef21123b1e6d02aab92fbb4cee4c022e2a3
Tory S. Anderson <torys.anderson@gmail.com>
2018-04-12 07:04:40 -0600

    dir cleaned

--- sql/init.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS conversations CASCADE;
-CREATE TABLE conversations (
-	id SERIAL PRIMARY KEY,
-        creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        origin_url TEXT UNIQUE,
-        full_text TEXT,
-        other_info JSONB);
