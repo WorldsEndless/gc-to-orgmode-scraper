b4de23a92fbb54e82191add5b2e8e895d2363727
Tory S. Anderson <torys.anderson@gmail.com>
2020-04-18 09:09:45 -0600

    gets everything, but structure of html results in doubles,
    one with correct name, one with the content

--- src/gc_scraper/general_conference.clj
+++ src/gc_scraper/general_conference.clj
@@ -17,9 +17,11 @@
     (catch Exception e (do (println "No author found")
                            "<no author>"))))
 
-(defn get-content [talk] (html/select talk [:div.body-block]))
+(defn get-content [talk] (html/select talk [:article]))
 
-(defn get-references [talk] (html/select talk [:footer.notes :ol]))
+;; DOESN'T WORK with new footnote format
+;; (defn get-references [talk] (html/select talk [:footer.notes :ol]))
+;;; 
 
 (defn make-filename [name]
   (-> name
@@ -27,14 +29,13 @@
       (str/replace #"[^a-zA-Z-]" "")))
 
 (defn pandoc-from-html
-  [{:keys [title html-content-string html-references-string author]}]
+  [{:keys [title html-content-string author]}]
   (println "Attempting to gen org from " title)
   (-> (clojure.java.shell/sh "pandoc" "-f" "html" "-t" "org"
                              "--wrap" "none"
                              :in (str
                                   "<h1>" title " (" author ")</h1>"
-                                  "<h2>Contents</h2>" html-content-string
-                                  "<h2>References</h2>" html-references-string))
+                                  "<h2>Contents</h2>" html-content-string))
       :out
       (str/replace #"(?s)#\+BEGIN_HTML.*?#\+END_HTML" "") ; Clear HTML markers for cleaner ORG
       (str/replace #" " " ")))
@@ -44,19 +45,22 @@
   (try (clojure.java.shell/sh "pandoc" "-v")
        (catch java.io.IOException _ false)))
 
-(defn gc [output-path & [talk-urls]]
+(defn gc
+  "Given an output dir and all the talk URLs, produce each file of pandoc results of the content of each talk"
+ [output-path & [talk-urls]]
   (if-not (pandoc?)
     (throw (ex-info "Pandoc not found on system" {:cause :no-pandoc}))
     (let [html-talks (map #(html/html-resource (URL. %)) talk-urls)
           single-output-file (str output-path "all.org")]
       (println "writing to " single-output-file)
-      (spit single-output-file "#+TITLE: General Conference October 2019\n") ;; clear the file first
+      (spit single-output-file "#+TITLE: General Conference April 2020\n") ;; clear the file first
       (doseq [talk html-talks]
         (let [enlive-html-content (get-content talk)
               org-doc (pandoc-from-html {:title (get-title talk)
                                          :author (get-author talk)
                                          :html-content-string (reduce str (html/emit* enlive-html-content))
-                                         :html-references-string (reduce str (html/emit* (get-references talk)))})]
+                                         ;:html-references-string (reduce str (html/emit* (get-references talk)))
+                                         })]
           (do
             (println "Processing " (get-title talk))
             (spit single-output-file org-doc :append true)))))))
@@ -64,12 +68,20 @@
 (defn get-web-gc
   "Get general conference from the website"
   [output-dir-path]
-  (let [index-url "https://www.lds.org/general-conference?lang=eng"
-        talk-urls (-> index-url URL. html/html-resource (html/select [:a.lumen-tile__link])
-                         (->> (map #(str "https://www.lds.org" (get-in % [:attrs :href])))))]
+  (let [domain "https://www.churchofjesuschrist.org"
+        conference-substring "/study/general-conference/2020/04"
+        lang "?lang=eng"
+        index-url (str domain conference-substring lang)
+        talk-urls (-> index-url URL. html/html-resource
+                      (html/select [:li :a])
+                      (->> (map #(str domain (get-in % [:attrs :href])))
+                           (filter #(re-find
+                                     (re-pattern
+                                      (str conference-substring "/"))
+                                     %))))]
     (gc output-dir-path ; TODO requires a slash at the end, right now
         talk-urls)))
                                         
 #_(do 
-  (def output-dir-path "/home/torysa/Documents/Gospel_Files/General_Conference/20192/")
+  (def output-dir-path "/home/torysa/Documents/Gospel_Files/General_Conference/2020-1/")
   (get-web-gc output-dir-path))
