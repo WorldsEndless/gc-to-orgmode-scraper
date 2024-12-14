3c5e20f8fdeab5b7e30ad97e93805d74787a6ce6
Tory Anderson <torys.anderson@gmail.com>
2024-12-14 11:42:07 -0700

    updated clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Web-scraping and conversion of LDS General Conference to ORG mode using Pandoc"
   :url "http://tech.toryanderson.com"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"] ;; old, but I haven't got around to healing with all the transitive effects of upgrading this
+  :dependencies [[org.clojure/clojure "1.12.0"] 
                  [enlive "1.1.6"] ;; the HTML handling library
                  ;;;;;;;; 
                  ;; [cheshire "5.8.0"] for dealing with json, not needed here
